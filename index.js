eae107c5778986b0821e15f7b6d375bf97367e34
Michelle Vitello <michelleramona89@gmail.com>
2022-09-18 10:53:27 -0400

    commit tree cca9e39c1c48b77dba69edd470a944fcd4d3e97b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array, finder){
+    let result = null
+    for (let item of array){
+        if (finder(item)=== "W"){
+            result = item
+            break}
+    }
+    return result
+}
